8501b3aa9f9fa6100369b8dc9621622fad45eec3
Roger Tosbotn <rogertosb@gmail.com>
2020-06-30 11:18:46 +0200

    created test users for vero, thib, rog

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+
+
+#Test users
+
+Thibuser = User.create!(email: "thib@jaja.com", password: "jajajaja")
+Verouser = User.create!(email: "vero@jaja.com", password: "jajajaja")
+Roguser = User.create!(email: "rog@jaja.com", password: "jajajaja")
+
+
